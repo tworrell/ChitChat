7c0fcc7928fd70450097e7d2dd7811f7532e8a23
tworrell <tara.worrell@gmail.com>
2020-07-16 15:32:44 -0400

    updated page with the proper components needed to render the project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import Chatbot from 'react-chatbot-kit'
+import Chatbot from 'react-chatbot-kit';
 import './App.css';
+
+import ActionProvider from './ActionProvider';
+import MessageParser from './MessageParser';
+import config from './config';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Chatbot />
+        <Chatbot config={config} actionProvider={ActionProvider}  messageParser={MessageParser} />
       </header>
     </div>
   );
